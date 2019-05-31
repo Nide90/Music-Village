6f81035560837a88f440b332dbd8d8a9ee4fba11
mat1asl0pez <49660976+mat1asl0pez@users.noreply.github.com>
2019-05-31 19:17:26 -0300

    Add files via upload

---
+++ js/signin.js
@@ -0,0 +1,10 @@
+//el hide y show de los terminos//
+
+$(document).ready(function() {
+    $("#link1").hide();
+ });
+   $(document).ready(function() {
+    $("#showme1").click(function(){  
+        $("#link1").toggle();
+     });  
+});  
